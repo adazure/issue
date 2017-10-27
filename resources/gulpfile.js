93a7302a2d73356155688d1c772bd1784fdab100
Kerem Yavuz <=>
2017-10-27 17:18:40 +0300

    Güncelleme

--- resources/gulpfile.js
+++ resources/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var concat = require("gulp-concat");
 var cssmin = require("gulp-cssmin");
 var sass = require('gulp-sass');
+var jsmin = require('gulp-jsmin');
 
 // Client için olan sass ve script dosyaları
 var sass_client_path = './client/sass/*.scss';
@@ -11,6 +12,14 @@ gulp.task('sass-client', function() {
         .pipe(concat('issue-client-min.css'))
         .pipe(cssmin())
         .pipe(gulp.dest('../app/client/assets/css'));
+});
+
+var script_client_path = './client/scripts/*.js';
+gulp.task('script-client', function() {
+    return gulp.src(script_client_path)
+        .pipe(concat('issue-client-min.js'))
+        .pipe(jsmin())
+        .pipe(gulp.dest('../app/client/assets/scripts'));
 });
 
 
@@ -25,7 +34,8 @@ gulp.task('sass-server', function() {
 });
 
 
-gulp.task('default', ['sass-client', 'sass-server'], function() {
+gulp.task('default', ['sass-client', 'sass-server', 'script-client'], function() {
     gulp.watch(sass_client_path, ['sass-client']);
+    gulp.watch(script_client_path, ['script-client']);
     gulp.watch(sass_server_path, ['sass-server']);
 });
